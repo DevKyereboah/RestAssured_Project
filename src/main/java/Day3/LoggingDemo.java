7863636ac40cd8f0167748b47d48077e25ce8def
DevKyereboah <117347003+DevKyereboah@users.noreply.github.com>
2025-04-07 16:09:44 +0000

    Added loggings

--- src/main/java/Day3/LoggingDemo.java
+++ src/main/java/Day3/LoggingDemo.java
@@ -1,4 +1,21 @@
 package Day3;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class LoggingDemo {
+    @Test
+    public void testlogs(){
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+
+                .then()
+                //.log().body();
+                //.log().cookies();
+                //.log().headers();
+                .log().all();
+
+    }
 }
